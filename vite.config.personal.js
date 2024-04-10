b9e646d6db000b69e447cabff87e08339284f1b9
Rafał Borowski <moderator9781@gmail.com>
2024-04-10 14:38:01 +0200

    Move public directrory to src and clean config

--- vite.config.personal.js
+++ vite.config.personal.js
@@ -1,4 +1,3 @@
-import { fileURLToPath, URL } from 'node:url'
 import { resolve } from 'path';
 
 import { defineConfig } from 'vite'
@@ -6,24 +5,32 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   root: "./src/applications/personal",
-  //base: "",
+  root: "./src/applications/personal", //where index.html entry file located
+  publicDir:"../../../public",
   build: {
-    outDir: '../../../dist/personal', // Katalog docelowy dla plików zbudowanych
+    outDir: '../../../dist/personal', // Target catalog for build
     emptyOutDir: true,
-    //emptyOutDir: false, // Wyłączenie czyszczenia katalogu wynikowego przed budowaniem
-
-    // Konfiguracja wejść (punkty wejścia) dla budowania
-    //rollupOptions: {
-      //input: 'src/applications/personal/index.html', // Punkt wejścia dla aplikacji personal
-      // Definiujemy wyjścia dla każdej aplikacji
-      // output: {
-      //   dir: "dist",
-      //   entryFileNames: '[name].[hash].js', // Nazwa pliku wejściowego
-      //   chunkFileNames: 'assets/[name].[hash].js', // Katalog dla plików chunków
-      //   assetFileNames: 'assets/[name].[hash].[ext]', // Katalog dla plików zasobów
-      // }
-    //}
+    rollupOptions: {
+      input: {personal: "./src/applications/personal/index.html"},
+      output: { // here define naming pattern and directories for assets
+        chunkFileNames: 'assets/js/[name]-[hash].js', 
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        
+        assetFileNames: ({name}) => {
+          if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')){
+              return 'assets/images/[name]-[hash][extname]';
+          }
+          
+          if (/\.css$/.test(name ?? '')) {
+              return 'assets/css/[name]-[hash][extname]';   
+          }
+ 
+          // default value
+          // ref: https://rollupjs.org/guide/en/#outputassetfilenames
+          return 'assets/[name]-[hash][extname]';
+        },
+      },
+    }
   },
   server: {
     port: 4000,
@@ -36,8 +43,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'), // Alias dla plików wspólnych
-      //'@app': resolve(__dirname, 'src/applications/personal'), // Alias dla aplikacji personal
+      '@': resolve(__dirname, 'src'), // Alias for common assets and components
+      '@app': resolve(__dirname, 'src/applications/personal'), // Alias for application resources
     }
   }
 })
